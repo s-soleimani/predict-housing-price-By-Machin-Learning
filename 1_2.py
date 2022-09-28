2cb437ed8cb67eaa92cede3679ef536f69183726
s-soleimani <roz.sorkh@gmail.com>
2022-09-28 09:59:40 +0330

    Update 1_2.py

--- 1_2.py
+++ 1_2.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[ ]:
-
-
-#1_2
 corr_matrix = data.corr()
 corr_matrix["median_house_value"].sort_values(ascending = False)
 features = ["median_house_value","median_income","total_rooms","housing_median_age"]
